ea9cdc10b4f8fffca7d08fff716a66a4664893ba
CICD-Tester <biocloud2022@gmail.com>
2024-08-12 16:27:35 -0400

    add graph to streamlit

--- sea_ad_single_cell_profiling_streamlit.py
+++ sea_ad_single_cell_profiling_streamlit.py
@@ -8,8 +8,8 @@ from collections import Counter
 from botocore import UNSIGNED
 from botocore.client import Config
 
-bucket_arn = "arn:aws:s3:::sea-ad-single-cell-profiling"
-bucket_name = bucket_arn.split(":")[-1]
+BUCKET_ARN = "arn:aws:s3:::sea-ad-single-cell-profiling"
+bucket_name = BUCKET_ARN.rsplit(':', maxsplit=1)[-1]
 
 # Initialize S3 client with unsigned config for public access
 s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
@@ -22,21 +22,22 @@ data_sources = Counter()
 file_types = Counter()
 donor_ids = set()
 
-for obj in response["Contents"]:
-    key = obj["Key"]
-    parts = key.split("/")
+if 'Contents' in response:
+    for obj in response["Contents"]:
+        key = obj["Key"]
+        parts = key.split("/")
 
-    file_extensions[key.split(".")[-1].lower()] += 1
+        file_extensions[key.split(".")[-1].lower()] += 1
 
-    if len(parts) >= 1 and parts[0] in ["DLPFC", "MTG"]:
-        data_sources[parts[0]] += 1
+        if len(parts) >= 1 and parts[0] in ["DLPFC", "MTG"]:
+            data_sources[parts[0]] += 1
 
-    if len(parts) >= 2 and parts[1] in ["RNAseq", "ATACseq"]:
-        file_types[parts[1]] += 1
+        if len(parts) >= 2 and parts[1] in ["RNAseq", "ATACseq"]:
+            file_types[parts[1]] += 1
 
-    if "_SEAAD_" in key:
-        donor_id = key.split("_SEAAD_")[0]
-        donor_ids.add(donor_id)
+        if "_SEAAD_" in key:
+            donor_id = key.split("_SEAAD_")[0]
+            donor_ids.add(donor_id)
 
 def format_summary(summary_dict):
     formatted_text = ""
@@ -61,6 +62,9 @@ def create_streamlit_app(files, summaries):
 
     st.header("File List")
     st.write(files)
+
+    st.subheader("File Extensions Count")
+    st.bar_chart(summaries["file_extensions"])
 
 if __name__ == "__main__":
     response = s3.list_objects_v2(Bucket=bucket_name)
