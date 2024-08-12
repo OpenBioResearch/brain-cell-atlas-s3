e05d138e58f74087b39013ac4861f41c986d97ae
CICD-Tester <biocloud2022@gmail.com>
2024-08-12 12:46:17 -0400

    create csv

--- sea_ad_single_cell_profiling_files.py
+++ sea_ad_single_cell_profiling_files.py
@@ -1,11 +1,14 @@
+"""
+Lists object in the AWS S3 bucket using an unsigned request,
+and saves the keys to a CSV file.
+"""
+
 import boto3
 from botocore import UNSIGNED
 from botocore.client import Config
+import csv
 
 def list_s3_objects(bucket_arn):
-    """
-    Simple SEA-AD S3 bucket list. AWS has now created a bucket browser.
-    """
     try:
         bucket_name = bucket_arn.split(":")[-1]
         s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
@@ -20,8 +23,13 @@ def list_s3_objects(bucket_arn):
 
 bucket_arn = "arn:aws:s3:::sea-ad-single-cell-profiling"
 objects = list_s3_objects(bucket_arn)
+
 if objects:
-    for obj in objects:
-        print(obj)
+    with open('s3_objects_list.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Object Key"])
+        for obj in objects:
+            writer.writerow([obj])
+    print("CSV file has been created successfully.")
 else:
     print("No objects found in the bucket.")
